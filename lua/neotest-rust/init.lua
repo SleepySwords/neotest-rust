0954ffd15a9eeeb052f9f06c218a520fab1d99f2
Swords <33922797+SleepySwords@users.noreply.github.com>
2022-07-09 20:48:06 +1000

    feat: basic functionality (ie: only shows if a test has passed or failed)

--- lua/neotest-rust/init.lua
+++ lua/neotest-rust/init.lua
@@ -14,10 +14,7 @@ local NeotestAdapter = { name="neotest-rust" }
 ---@async
 ---@param dir string @Directory to treat as cwd
 ---@return string | nil @Absolute root dir of test suite
-function NeotestAdapter.root(dir)
-  -- async.api.nvim_command('echo ' .. vim.inspect(lib.files.match_root_pattern("Cargo.lock")(dir)))
-  return lib.files.match_root_pattern("Cargo.lock")(dir)
-end
+NeotestAdapter.root = lib.files.match_root_pattern("Cargo.lock")
 
 ---@async
 ---@param file_path string
@@ -27,6 +24,25 @@ function NeotestAdapter.is_test_file(file_path)
     return false
   end
     return true
+end
+
+-- Holy shit lua how do you not have an escape string function
+local function escape_pattern(text)
+    return text:gsub("([^%w])", "%%%1")
+end
+
+-- Literally the most sketch thing
+local function extract_module(path)
+  local root = NeotestAdapter.root(path)
+  path = path:gsub('^' .. escape_pattern(root .. '/src/'), '')
+  path = path:gsub('/mod.rs$', '')
+  -- should be path seperator but oh well
+  path = path:gsub('/', '::')
+  path = path:gsub('%.rs$', '')
+  if path == 'main' then
+      return nil
+  end
+  return path:gsub('%.rs$', '')
 end
 
 ---Given a file path, parse all the tests within it.
@@ -47,35 +63,58 @@ function NeotestAdapter.discover_positions(file_path)
 	  @namespace.definition
     ]]
   return lib.treesitter.parse_positions(file_path, query, {
-      require_namespaces = false,
-      position_id = function (position, namespaces)
-        async.api.nvim_command('echo ' .. vim.inspect(position) .. ' ' .. vim.inspect(namespaces))
-        return table.concat(
-          vim.tbl_flatten({
-            position.path,
-            vim.tbl_map(function(pos)
-              return pos.name
-            end, namespaces),
-            position.name,
-          }),
-          "::"
-        )
-      end
+    require_namespaces = false,
+    position_id = function(position, namespaces)
+      return table.concat(
+        vim.tbl_flatten({
+          extract_module(position.path),
+          vim.tbl_map(function(pos)
+            return pos.name
+          end, namespaces),
+          position.name,
+        }),
+        "::"
+      )
+    end,
   })
+end
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
 end
 
 ---@param args neotest.RunArgs
 ---@return neotest.RunSpec
 function NeotestAdapter.build_spec(args)
+
+  -- TODO: handle files and directories
   local position = args.tree:data()
-  async.api.nvim_command('echo ' .. vim.inspect(position.id))
   local results_path = async.fn.tempname()
   local command = vim.tbl_flatten({
     "cargo", "test", "--",
     "--logfile",
     results_path,
-    vim.list_extend(args.extra_args or {})
+    args.extra_args or {}
   })
+  if position then
+    table.insert(command, position.id)
+  end
+  -- TODO: add DAP support
+  return {
+    command = command,
+    context = {
+      results_path = results_path,
+    }
+  }
 end
 
 ---@async
@@ -83,6 +122,31 @@ end
 ---@param result neotest.StrategyResult
 ---@param tree neotest.Tree
 ---@return table<string, neotest.Result>
-function NeotestAdapter.results(spec, result, tree) end
+function NeotestAdapter.results(spec, r, tree)
+  -- local success, data = pcall(lib.files.read, result.output)
+  -- if not success then
+  --   return {}
+  -- end
+  -- TODO: show the error when failed
+  local success, data = pcall(lib.files.read, spec.context.results_path)
+  if not success then
+    data = "{}"
+  end
+  local results = {}
+  local lines = vim.split(data, '\n')
+  for _, line in ipairs(lines) do
+    if line ~= '' then
+      local result = vim.split(line, ' ')
+      local status = 'skipped'
+      if result[1] == 'ok' then
+        status = 'passed'
+      elseif result[1] == 'failed' then
+        status = 'failed'
+      end
+      results[result[2]] = { status = status }
+    end
+  end
+  return results
+end
 
 return NeotestAdapter
